1816f67d5a32ec8332d44f3fc28944d7f51d3be3
Danny <walshdanny700@gmail.com>
2021-03-01 14:14:44 +0000

    QuizApiApplication.java added extends for class SpringBootServletInitializer to fix issue with aws

--- quiz-api/src/main/java/com/company/quizapi/QuizApiApplication.java
+++ quiz-api/src/main/java/com/company/quizapi/QuizApiApplication.java
@@ -1,14 +1,14 @@
 package com.company.quizapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.Bean;
-import org.modelmapper.ModelMapper;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-public class QuizApiApplication {
+public class QuizApiApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(QuizApiApplication.class, args);
